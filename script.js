19893b77de41d41304351afa5ffd41316f16b2fd
giotsere <giorgitsereteli@protonmail.com>
2021-02-20 01:40:00 +0200

    Html struct updated

---
+++ script.js
@@ -0,0 +1,17 @@
+let choices = ["Rock","Paper","Scissors"]
+
+
+function computerPlay(){
+    const random = Math.floor(Math.random() * choices.length)
+    return choices[random];
+}
+
+function playRound(playerSelection, computerSelection){
+
+}
+
+
+function playerPlay(){
+    
+}
+
